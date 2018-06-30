3fabf49ae02b61c4174769ae798cd45cd40bdcba
Stephen Afolabi <stephenafolabi@gmail.com>
2018-06-30 20:01:04 +0000

    changed to local

--- chestergreen_elect/settings/base.py
+++ chestergreen_elect/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
 ]
 
 MIDDLEWARE = [
@@ -102,4 +103,4 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_URL = '/static/'
