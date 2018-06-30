6413f39604e9162f10c670abc59ce8aa51d2343f
Stephen Afolabi <stephenafolabi@gmail.com>
2018-06-30 18:45:01 +0000

    prepared for heroku

--- chestergreen_elect/settings/base.py
+++ chestergreen_elect/settings/base.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=2r4#ouv5f^b&+_!63&50tw_dz7hhr+j9yg0vhcig01%)yo#gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chestergreen_elect.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
@@ -114,7 +103,3 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
---
+++ chestergreen_elect/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'x-mp#z^%xv^m7)_v=^rw%0ie20*criwn&ayb6-shi=+2g*9)b+'
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SYSTEM_EMAIL = 'yourname@example.com'
