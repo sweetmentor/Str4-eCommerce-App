e50edff9183a207c8e7daff16136059fcb5f9002
Stephen Afolabi <stephenafolabi@gmail.com>
2019-01-29 01:13:56 +0000

    debuged profile image display problem

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Profile")
-    description = models.TextField(max_length=50, default='', null = False, blank = False)
+    description = models.TextField(max_length=50, default='unknown', null = False, blank = False)
     image = models.ImageField(upload_to="avatars", null=False, default='no_image.jpg')
     
     def __str__(self):
